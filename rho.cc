2d970ffdba229c3b8a3c506e9314da58a76f177c
yarziz <yarziz-houssam12@hotmail.fr>
2020-11-20 22:46:47 +0000

    first

--- rho.cc
+++ rho.cc
@@ -27,8 +27,37 @@ double u_seconde(vector<double> x,vector<double> rho, int j){
   double a=(rho[j+1]-rho[j])/(x[j+1]-x[j]);
   double b=(rho[j-1]-rho[j])/(x[j]-x[j-1]);
   return 2*(a+b)/(x[j+1]-x[j-1]);
-  
 }  
+
+vector<vector<double>> remplissage(int n, vector<double> x, double c, double v, double min){
+  vector<double> kr(n+1);
+  vector<double> metr(n+1);
+  vector<vector<double>> kk(n+1,vector<double>(n+1,0.));
+  
+  for(int j=0;j<n+1;j++){
+    metr[j]=max(abs(u_seconde(x[j],c,v)),min);
+    // cout<<"vl"<<abs(u_seconde(x_j,c,v))<<endl;
+  }
+
+  //cout<<"----------------------------------------------"<<endl;
+
+  kr[0]=0.;
+  for(int k=1;k<n+1;k++){
+    kr[k]=(metr[k-1]+metr[k])/2.;
+  }
+
+  kk[0][0]=pow(10,12);
+  kk[n][n]=pow(10,12);
+  for(int i=1;i<n;i++){
+    kk[i][i]=(kr[i]+kr[i+1]);  
+  }
+
+  for(int i=1;i<n;i++){
+    kk[i][i+1]=-kr[i+1];
+    kk[i][i-1]=-kr[i];
+  }
+  return kk;
+}
 
 
 
@@ -43,6 +72,9 @@ int main(){
   double dt(0), dx(0);
   vector<double> x(n+1,0.),b(n+1,0.),xn(n+1,0.);
   vector<double> rho(n+1),rhon(n+1);
+  vector<vector<double>> kk(n+1,vector<double>(n+1,0.));
+
+  
   dt=10/(p*1.);
   dx=0.01/(n*1.);
   ofstream mon_flux;
@@ -65,10 +97,8 @@ int main(){
     name_file="Result"+to_string(k);
     mon_flux.open(name_file, ios::out);
     for(int z=0; z<zmax+1;z++ ){
-      for(int k=0;k<n;k++){
-	rhon[k]=(1-e(k*dx,j*dt,dt))*rho[k]+1000*e(k*dx,j*dt,dt);
-	mon_flux << xn[k] << " " << rhon[k] << endl;
-	//mon_flux << j*dt << " " << brhon << endl;
+      for(int k=0;k<n+1;k++){
+	rhon[k]=(1-e(x[k],j*dt,dt))*rho[k]+1000*e(x[k],j*dt,dt);
       }
       kk=remplissage(n,x,min);
       xn=resol(kk,b,n);
@@ -78,7 +108,9 @@ int main(){
       }
       x=xn;
     }
-   
+    for(int k=0;k<n+1;k++){
+      mon_flux << xn[k] << " " << rhon[k] << endl;
+    }
     rho=rhon;
     mon_flux.close();
 
